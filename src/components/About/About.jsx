283a0f17780e5a7bf1ff972051044d272ca4a05c
miacsilva <miacsilvac@hotmail.com>
2023-11-23 05:09:55 +0000

    about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,23 @@
 import "./about.css"; 
-import maria from "/maria-portrait.PNG"
+import maria from "/maria-portrait.png"
 
 function About() {
   return (
     <section id="about"> 
-    <h1>Get to know me!</h1>
 
 <div className="about-content">
     <div className="about-text">
+
+    <h1>.getToKnowMe</h1>
       <h3>
         I'm Júnior Full-Stack Web Developer from Braga, Portugal. 
         My academic background is in Sociology where I learned to integrate in my life very interesting lens apon the world.
-        But something was missing, I felt like I needed to be able to create and be creative in a field I've always loved and felt is so relevant: Tech. 
-        My life changed when I engaged in a intensive Javascript Full-Stack Web Development Bootcamp and gained the tools to create big and beautiful things!
+        <span className="missing"> But something was missing.</span> I felt like I needed to be able to create and be creative in a field I've always loved and felt is so relevant: Tech. 
+        My life changed when I engaged in a intensive <span className="javascript">Javascript</span> Full-Stack Web Development Bootcamp and gained the tools to create big and beautiful things!
       </h3>
-    </div>
+      <h3>I'm into coding, music and nature 🧘</h3>
+  
+    </div >
     <img src={maria} alt="Maria Carvalho's portrait" className="portrait"/>
     </div>
     </section>
