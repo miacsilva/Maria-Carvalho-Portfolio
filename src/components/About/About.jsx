c3e2b7078edb72085dd991725249b17af4b701db
miacsilva <miacsilvac@hotmail.com>
2023-11-23 03:35:27 +0000

    responsive homepage and navbar

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,22 @@
 import "./about.css"; 
-/* import maria from "/src/assets/maria-portrait.png" */
+import maria from "/maria-portrait.PNG"
 
 function About() {
   return (
     <section id="about"> 
-   {/*  <img src={maria} alt="" /> */}
+    <h1>Get to know me!</h1>
+
+<div className="about-content">
+    <div className="about-text">
+      <h3>
+        I'm Júnior Full-Stack Web Developer from Braga, Portugal. 
+        My academic background is in Sociology where I learned to integrate in my life very interesting lens apon the world.
+        But something was missing, I felt like I needed to be able to create and be creative in a field I've always loved and felt is so relevant: Tech. 
+        My life changed when I engaged in a intensive Javascript Full-Stack Web Development Bootcamp and gained the tools to create big and beautiful things!
+      </h3>
+    </div>
+    <img src={maria} alt="Maria Carvalho's portrait" className="portrait"/>
+    </div>
     </section>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,64 @@
 import "./navbar.css"; 
+import { useState } from 'react'
+import {BiHomeAlt} from "react-icons/bi"
+import { TiUser } from "react-icons/ti";
+import {MdWork} from "react-icons/md"
+import { AiOutlineMessage } from "react-icons/ai";
 
 function Navbar() {
+  const [activeNav, setActiveNav] = useState("#")
   return (
-    <section id="navbar">
+    <section >
 
-    <div>
+<div id="navbar-desktop">
+<div >
     <a href="/" className="nav-Links maria">{"<Maria.dev />"}</a>
     </div>
 
     <div className="navegation">
     <a href="/#about" className="nav-Links nav-Links2">About Me</a>
     <a href="/#projects" className="nav-Links nav-Links2">Projects</a>
-    <a href="/#contact" className="nav-Links nav-Links2">Contact</a>
+    <a href="/#contact" className="nav-Links nav-Links2 last-Link">Contact</a>
     </div>
+</div>
 
+<nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <BiHomeAlt />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+      <TiUser/>
 
+      </a>
+
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#projects")}
+        className={activeNav === "#projects" ? "active" : ""}
+      >
+        <MdWork />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <AiOutlineMessage />
+      </a>
+    </nav>
+  
     </section>
   )
 }
+
+
 
 export default Navbar;
