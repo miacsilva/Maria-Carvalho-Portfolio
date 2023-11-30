b14298069e3cc81f24925d740e6134ac0c84bd42
miacsilva <miacsilvac@hotmail.com>
2023-11-30 20:49:39 +0000

    skills labelling

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -9,6 +9,9 @@ import { SiExpress } from "react-icons/si";
 import { SiAdobeillustrator } from "react-icons/si";
 import { SiMongodb } from "react-icons/si";
 import { FaBootstrap } from "react-icons/fa";
+import { ImGit } from "react-icons/im";
+import { SiNetlify } from "react-icons/si";
+import { VscGithub } from "react-icons/vsc";
 
 function Skills() {
   return (
@@ -19,16 +22,49 @@ function Skills() {
         <div className="skills-container">
           <div className="skill-type">
 
-          
+
             <p className="title">Frontend</p>
 
             <p className="skills-icons">
-              <FaHtml5 />
-              <FaCss3Alt />
-              <RiJavascriptFill />
-              <FaReact />
-              <FaBootstrap />
-              <TbApi />
+
+            <div id="double-set-icons">
+              <span className="icon-description">HTML</span>
+              <FaHtml5 className="icon"  />
+              </div>
+
+              <div id="double-set-icons">
+              <span className="icon-description">CSS</span>
+              <FaCss3Alt className="icon" />
+              </div>
+
+              <div id="double-set-icons">
+              <span className="icon-description">Javascript</span>
+              <RiJavascriptFill className="icon"/>
+              </div>
+
+             
+
+              <div id="double-set-icons">
+              <span className="icon-description">React</span>
+              <FaReact className="icon"/>
+              </div>
+             
+
+              <div id="double-set-icons">
+              <span className="icon-description">Bootstrap</span>
+              <FaBootstrap className="icon"/>
+              </div>
+             
+
+              <div id="double-set-icons">
+              <span className="icon-description">Rest API</span>
+              <TbApi className="icon" />
+              </div>
+
+             
+             
+              
+         
             </p>
           </div>
 
@@ -36,10 +72,31 @@ function Skills() {
             <p className="title">Backend</p>
 
             <p className="skills-icons">
-              <RiJavascriptFill />
-              <SiNodedotjs />
-              <SiExpress />
-              <SiMongodb />
+
+            <div id="double-set-icons">
+              <span className="icon-description">Javascript</span>
+              <RiJavascriptFill className="icon"/>
+              </div>
+
+            <div id="double-set-icons">
+              <span className="icon-description">Node.Js</span>
+              <SiNodedotjs className="icon"/>
+              </div>
+              
+            <div id="double-set-icons">
+              <span className="icon-description">Express</span>
+              <SiExpress className="icon"/>
+              </div>
+
+            <div id="double-set-icons">
+              <span className="icon-description">MongoDB</span>
+              <SiMongodb className="icon"/>
+              </div>
+
+              
+          
+              
+         
             </p>
           </div>
 
@@ -47,7 +104,35 @@ function Skills() {
             <p className="title">Other</p>
 
             <p className="skills-icons">
-              <SiAdobeillustrator />
+
+            <div id="double-set-icons">
+              <span className="icon-description">Adobe Illustrator</span>
+              <SiAdobeillustrator className="icon"/>
+              </div>
+
+           
+            <div id="double-set-icons">
+              <span className="icon-description">Git</span>
+              <ImGit className="icon"/>
+              </div>
+
+           
+           
+            <div id="double-set-icons">
+              <span className="icon-description">Netlify</span>
+              <SiNetlify className="icon" />
+              </div>
+
+           
+            <div id="double-set-icons">
+              <span className="icon-description">Github</span>
+              <VscGithub className="icon"/>
+              </div>
+
+           
+              
+            
+            
             </p>
           </div>
         </div>
