31bd28fa2225d9d8cea6a55305b2a8b4cbd1b4c0
miacsilva <miacsilvac@hotmail.com>
2023-12-15 16:02:45 +0000

    overflow solved

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,13 +2,13 @@ import "./projects.css";
 import { useEffect } from "react";
 import portfolioData from "/src/data/portfolioData.json";
 import Carousel from "react-bootstrap/Carousel";
-import Aos from "aos"
-import "aos/dist/aos.css"
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function Projects() {
-  useEffect(()=>{
-    Aos.init()
-      },[])
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return (
     <section id="projects">
       <div className="projects-container">
@@ -17,26 +17,31 @@ function Projects() {
         <div className="portfolio-container">
           {portfolioData.map(
             ({ id, images, title, description, github, live }) => {
-           
               return (
-                <article key={id} className="project-card" data-aos="zoom-in" data-aos-duration="1000">
-                  <div className="">
-                  <Carousel>
-                    {images.map((image, index) => (
-                      <Carousel.Item key={index} interval={6000}>
-                        <img
-                          src={image}
-                          className="d-block w-100 img-carousel"
-                          alt={`Slide ${index + 1}`}
-                        />
-                    
-                      </Carousel.Item>
-                    ))}
-                  </Carousel>
-                  </div>
+                <article
+                  key={id}
+                  className="project-card"
+                  data-aos="zoom-in"
+                  data-aos-duration="1000"
+                >
+                  <div>
+                    <Carousel>
+                      {images.map((image, index) => (
+                        <Carousel.Item key={index} interval={6000}>
+                          <img
+                            src={image}
+                            className="d-block w-100 img-carousel"
+                            alt={`Slide ${index + 1}`}
+                          />
+                        </Carousel.Item>
+                      ))}
+                    </Carousel>
+
                   <h3 className="card-title">{title}</h3>
-                  <p  className="card-description">{description}</p>
-                  <div className="">
+                  <p className="card-description">{description}</p>
+                  </div>
+
+                  <div className="card-buttons">
                     <a href={github} className="card-button">
                       GitHub
                     </a>
