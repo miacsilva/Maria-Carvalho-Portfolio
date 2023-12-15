6f7f89223fa67e83a6a4187b853fe2fea55de227
miacsilva <miacsilvac@hotmail.com>
2023-12-15 16:43:11 +0000

    about text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,12 +18,12 @@ function About() {
     <div className="about-text">
 
       <h3>
-        >>My name is Maria Carvalho and I'm Júnior Full-Stack Web Developer from Braga, Portugal.
-        My academic background is Sociology where I learned to integrate in my life very interesting lens apon the world.
-        <span className="missing"> But something was missing.</span> I felt like I needed to be able to create and be creative in a field I've always loved and felt is so relevant: Tech. 
-        My life changed when I engaged in a intensive <span className="javascript">Javascript</span> Full-Stack Web Development Bootcamp and gained the tools to create big and beautiful things!
+        >> My name is Maria Carvalho and I'm Junior Full-Stack Web Developer from Braga, Portugal.
+        Coming from a Sociology academic background, I learned to integrate in my life a very interesting lens upon the world.
+        However <span className="missing"> something was missing.</span> I felt like I needed to be able to create and be creative in a field I've always loved and felt is so relevant: Tech. 
+        My life changed when I engaged in a intensive <span className="javascript">Javascript</span> Full-Stack Web Development Bootcamp and embarked on this journey that gave me the tools to extrapolate from my code big and beautiful things!
       </h3>
-      <h3>Im into coding, music and nature.</h3>
+      <h3>I'm into coding, music and nature.</h3>
   
     </div>
 
